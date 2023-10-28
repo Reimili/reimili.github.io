0cca48e5034217b35e0bd87e60e7f5752d071406
Reimili <144122886+Reimili@users.noreply.github.com>
2023-10-28 12:35:39 +0300

    Add files via upload

--- index6/js.js
+++ index6/js.js
@@ -1,17 +1,80 @@
-window.onload = function () {
-	let btn_price = document.getElementById("button");
-	btn_price.addEventListener("click", function (e) {
-		e.preventDefault();
-		let good = document.getElementById("product");
-		let count = document.getElementById("number").value;
-		let priceEl = good.options[good.selectedIndex];
-		let price = priceEl.getAttribute("data-price");
-		let price_field = document.getElementById("sum");
+let prod1 = document.getElementById("prod1");
+let prod2 = document.getElementById("prod2");
+let prod3 = document.getElementById("prod3");
+let selection = document.getElementById("selection");
+let option = document.getElementById("option");
+let check = document.getElementById("check");
+let result = document.getElementById("result");
+let amountField = document.getElementById("amount");
+let selectedProduct = prod1;
 
-		if (/^\d+$/.test(count)) {
-			price_field.innerHTML = price * count + "₽";
-		} else {
-			price_field.innerHTML = "Введите корректное число товаров";
-		}
-	});
-};
+prod1.addEventListener("click", function () {
+    selection.className = "invisible";
+    option.className = "invisible";
+    check.checked = false;
+    selectedProduct = prod1;
+    resetResult();
+});
+prod2.addEventListener("click", function () {
+    selection.className = "visible";
+    option.className = "invisible";
+    check.checked = false;
+    selectedProduct = prod2;
+    resetResult();
+});
+prod3.addEventListener("click", function () {
+    selection.className = "invisible";
+    option.className = "visible";
+    check.checked = false;
+    selectedProduct = prod3;
+    resetResult();
+});
+
+function resetResult() {
+    result.textContent = "";
+}
+
+function calculatePrice() {
+    let amount = document.getElementById("amount").value;
+    let price = getProductPrice(selectedProduct.value);
+
+    if (!Number.isNaN(amount)) {
+        let parsedAmount = parseFloat(amount);
+        if (Number.isInteger(parsedAmount)) {
+            let cost = price;
+            if (check.checked) {
+                cost += 25;
+            }
+            if (selection.className !== "invisible") {
+                cost += getSelectPrice(selection.value);
+            }
+            cost *= parsedAmount;
+            result.textContent =  cost + " руб";
+        }
+    } else {
+        document.getElementById("result").textContent =
+        "Кол-во товара введено некорректно";
+    }
+}
+
+function getProductPrice(productName) {
+    switch (productName) {
+    case "Товар1":
+        return 39;
+    case "Товар2":
+        return 119;
+    case "Товар3":
+        return 89;
+    default:
+        return 0;
+    }
+}
+
+function getSelectPrice(selName) {
+    switch (selName) {
+    case "Опция1":
+        return 0;
+    case "Опция2":
+        return 15;
+    }
+}
