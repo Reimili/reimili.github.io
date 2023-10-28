1c7e24fcdc333b0878590a7275d33aac65881253
Reimili <144122886+Reimili@users.noreply.github.com>
2023-10-28 12:15:49 +0300

    Add files via upload

--- index5/js.js
+++ index5/js.js
@@ -1,17 +1,34 @@
-window.onload = function () {
-	let btn_price = document.getElementById("button");
-	btn_price.addEventListener("click", function (e) {
-		e.preventDefault();
-		let good = document.getElementById("product");
-		let count = document.getElementById("number").value;
-		let priceEl = good.options[good.selectedIndex];
-		let price = priceEl.getAttribute("data-price");
-		let price_field = document.getElementById("sum");
-
-		if (/^\d+$/.test(count)) {
-			price_field.innerHTML = price * count + "₽";
-		} else {
-			price_field.innerHTML = "Введите корректное число товаров";
+function calculatePrice() {
+	let selectedProduct = document.getElementById("product").value;
+	let amount = document.getElementById("amount").value;
+	let isNumber = /^[0-9]*(\.[0-9]+)?$/.test(amount);
+	let price = getProductPrice(selectedProduct);
+  
+	if (isNumber) {
+		let parsedAmount = parseFloat(amount);
+		if (Number.isInteger(parsedAmount)) {
+			let cost = price * parsedAmount;
+			document.getElementById("result").textContent = cost + " руб";
 		}
-	});
-};
+	} else {
+		document.getElementById("result").textContent =
+		"Неверное количество товара";
+	}
+  }
+  
+  function getProductPrice(productName) {
+	switch (productName) {
+	case "1":
+		return 89;
+	case "2":
+		return 32;
+	case "3":
+		return 10;
+	case "4":
+		return 0;
+	case "5":
+		return 37890;
+	default:
+		return 0;
+	}
+  }
